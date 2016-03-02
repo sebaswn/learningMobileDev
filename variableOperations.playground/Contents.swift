edcf10916d43326e40f3af348709cc7666e68984
sebaswn <sebastiannee@yahoo.com>
2016-03-02 14:39:16 +0100

    Lecture 14 Variable Operations

---
+++ variableOperations.playground/Contents.swift
@@ -0,0 +1,57 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+Create a variable with a name of your choosing that has a value of 15
+Create a 2nd variable with a name of your choosing that has a value of 5
+Create a 3rd variable that stores that sum of the 1st and 2nd variables (20 should print to the screen)
+Create a 4th variable that stores the result of a multiplication operation - the stored result should be equal to 45
+Create a 5th variable that stores the result of the 1st variable minus the 2nd variable - the result should be 10
+Create a 6th variable that stores the result of the 1st variable divided by the 2nd variable - the result should be 3
+Create 3 new variables of any name and of any value, but all on the same line
+Create 3 constants (let) of any name and of any value, but all on the same line
+Create an explicit variable of any name of type Double and don't give it any value
+Create another explicit variable of any name of type Int but DO give it an initial value of 423
+Create a variable of type String with a value of "Hello"
+Create another variable of type String with a value of "World"
+Create a 3rd variable to store the result of concatenating the first two strings (using the + symbol)
+
+*/
+
+var firstInt = 15
+var secondInt = 5
+var sumOfInts = firstInt + secondInt
+
+var result = 5 * 5
+
+var subOfInts = firstInt - secondInt
+
+var divOfItns = firstInt / secondInt
+
+var one = 1, two = 2, three = 3
+one
+two
+three
+
+let four = 4, five = 5, six = 6
+four
+five
+six
+
+var decimalNum: Double
+
+var numberInt: Int = 423
+numberInt
+
+var firstString = "Hello"
+var secondString = "world!"
+
+var sumOfStrings = firstString + " " + secondString
+
+
+
+
+
+
+
