dc7a50c3d2399f0756b99ab79a7143545f1cddf1
sebaswn <sebastiannee@yahoo.com>
2016-03-02 12:43:25 +0100

    Lecture 12: Numbers and Type Safety

---
+++ numbers.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// integer: whole numbers with no fractions. + or - or 0
+
+var myBankAccount = -500
+
+var otherBankAccount: Int = -12
+
+var my2ndBankAccount: UInt = 200
+//UInt cannot have negatives. Only use when you are sure it is positive
+
+var myAge: UInt = 18
+
+//var max size: 2 147 483 647
+
+var maxNum: Int64 = 14896451674136752
+
+// var float goes to 6 decimals
+// var double goes to 15
+
+var realBankAccount: Double = 22.43
+
+var someVal: Float32 = 5.5
+
+//Double and Float cannot be added or multiplied to eachother
+
+var sum = realBankAccount * Double(someVal)
+
+// sum is double 
+
+
+
+
