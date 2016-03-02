b6f9bd37b8558bceb6ce17e5dd41e2f1fb22086c
sebaswn <sebastiannee@yahoo.com>
2016-03-02 12:18:24 +0100

    variables done

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -3,3 +3,40 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var number = 10
+
+var otherNumber = 10.5
+
+otherNumber = 20.4
+
+let newDecimal = 40.4
+
+// let cannot be changed
+// var can be 
+
+var 😎 = "cool"
+
+var cool = "😎"
+
+
+// multiple vars on one line
+var account1 = 90.0, account2 = -10, account3 = 1000000
+
+account1
+account2
+account3
+
+
+var newMessage: String
+//type annotation. Only string can go into this now
+
+newMessage = "Hi, This is a new message"
+
+var red, green, blue: Double
+
+red = 29
+green = 50.7
+
+
+
