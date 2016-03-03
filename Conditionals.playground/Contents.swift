c3f96290863493f704eff91b7a5e4a8331111de6
sebaswn <sebastiannee@yahoo.com>
2016-03-03 11:59:10 +0100

    Lecture 21: Conditionals

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,67 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var isMyHouseOnFire: Bool = false
+
+var anotherBool = true
+
+if isMyHouseOnFire {
+    print("Someone get me some water")
+}else {
+    print("Someone get some fire")
+}
+
+
+var result = true == true
+result = true == false
+result = false == false
+
+
+var accountTotal = 200.5
+var newVideoGame = 59.99
+
+if accountTotal >= newVideoGame{
+    print("I just purchesed the game.")
+    
+}else{
+    print("I don't have enough money.")
+}
+
+
+
+var name = "Jack"
+
+if name == "Jack" {
+    print("Your name is Jack")
+    
+}else{
+    print("Your name is not Jack")
+}
+
+
+var numberA = 25
+var numberB = 99
+var numberC = 50
+var finalNumber = 100
+
+if numberA == finalNumber{
+    print(numberA)
+}else if numberB == finalNumber{
+    print(numberB)
+}else if numberC != finalNumber{
+    print(numberC)
+}else {
+    print("none of the numbers are equal to final number")
+}
+
+
+
+
+
+
+
+
+
+
